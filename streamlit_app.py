6e1b9563a969d872a839e02afe1af75a0c24a201
anish johnson <anishjohnson05@gmail.com>
2022-05-23 10:30:33 +0530

    Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,64 +1,60 @@
-# Import the required libraries.
 import numpy as np
 import cv2
+from keras.models import load_model
 import streamlit as st
 from tensorflow import keras
-from keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
 
-# Load the model that we saved in h5 format.
-classifier =load_model('model_78.h5')
-
-# Define the emotion labels.
+# load model
 emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
 
-# Detect the face using open_cv.
+# load weights into new model
+classifier =load_model('model_78.h5')
+
+#load face
 try:
-    face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 except Exception:
     st.write("Error loading cascade classifiers")
 
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-# Now lets create the function that will detect faces and predict their emotions.
 class Faceemotion(VideoTransformerBase):
-    # Function to detect face and predict its emotion.
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
         #image gray
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        faces = face_classifier.detectMultiScale(gray, 1.32, 5)
-
-                 
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),4)
-            roi_gray = gray[y:y+h,x:x+w]
-            roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
-
-            if np.sum([roi_gray])!=0:
-                roi = roi_gray.astype('float')/255.0
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
                 roi = img_to_array(roi)
-                roi = np.expand_dims(roi,axis=0)
-
+                roi = np.expand_dims(roi, axis=0)
                 prediction = classifier.predict(roi)[0]
-                label=emotion_labels[prediction.argmax()]
-
-            label_position = (x,y-10)
-            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_labels[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         return img
 
-# App to deploy on streamlit.
 def main():
     # Face Analysis Application #
     st.title("Real Time Face Emotion Detection Application")
     activiteis = ["Home", "Webcam Face Detection", "About"]
     choice = st.sidebar.selectbox("Select Activity", activiteis)
     st.sidebar.markdown(
-        """ Developed by Anish Johnson    
-            Email : anishjohnson05@gmail.com 
-            [LinkedIn] (linkedin.com/in/anish-johnson-594110208)""")
+        """ Developed by Anish Johnson   
+            Email : anishjohnson05@gmail.com  
+            [LinkedIn](https://www.linkedin.com/in/anish-johnson-594110208/)""")
     if choice == "Home":
         html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
                                             <h4 style="color:white;text-align:center;">
@@ -67,13 +63,14 @@ def main():
                                             </br>"""
         st.markdown(html_temp_home1, unsafe_allow_html=True)
         st.write("""
-                    This web application is capable of detecting a users face using OpenCV 
-                    and predicting the emotion He/She is displaying.
+                 The application has two functionalities.
+                 1. Real time face detection using web cam feed.
+                 2. Real time face emotion recognization.
                  """)
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
         st.write("Click on start to use webcam and detect your face emotion")
-        webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV,
+        webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
                         video_processor_factory=Faceemotion)
 
     elif choice == "About":
@@ -87,7 +84,7 @@ def main():
 
         html_temp4 = """
                              		<div style="background-color:#98AFC7;padding:10px">
-                             		<h4 style="color:white;text-align:center;">This Application is developed by Anish Johnson using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at anishjohnson05@gmail.com. </h4>
+                             		<h4 style="color:white;text-align:center;">This Application is developed by Mohammad Anish Johnson using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at anishjohnson05@gmail.com. </h4>
                              		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
                              		</div>
                              		<br></br>
