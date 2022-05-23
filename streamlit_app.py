0c45e643141da8534132249f24f1536b6a891409
anish johnson <anishjohnson05@gmail.com>
2022-05-23 12:53:42 +0530

    streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,11 @@ classifier =load_model('model_78.h5')
 
 #load face
 try:
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    face_cascade = cv2.CascadeClassifier('cv2.data.haarcascades + "haarcascade_frontalface_default.xml')
 except Exception:
     st.write("Error loading cascade classifiers")
 
-RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-
-class Faceemotion(VideoTransformerBase):
+class VideoTransformer(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
@@ -29,35 +27,31 @@ class Faceemotion(VideoTransformerBase):
         faces = face_cascade.detectMultiScale(
             image=img_gray, scaleFactor=1.3, minNeighbors=5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),4)
-            roi_gray = img_gray[y:y+h,x:x+w]
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
             roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-
             if np.sum([roi_gray]) != 0:
                 roi = roi_gray.astype('float') / 255.0
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
-                
                 prediction = classifier.predict(roi)[0]
                 maxindex = int(np.argmax(prediction))
                 finalout = emotion_labels[maxindex]
                 output = str(finalout)
-            label_position = (x, y-10)
-
+            label_position = (x, y)
             cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         return img
 
 def main():
-    # Designing webpage layout. 
+    # Face Analysis Application #
     st.title("Real Time Face Emotion Detection Application")
-
-    activiteis = ["Home", "Face Emotion Detection", "About"]
-
+    activiteis = ["Home", "Webcam Face Detection", "About"]
     choice = st.sidebar.selectbox("Select Activity", activiteis)
     st.sidebar.markdown(
-        """ Developed by Anish Johnson   
-            Email : anishjohnson05@gmail.com  
+        """ Developed by Anish Johnson    
+            [Mail](anishjohnson05@gmail.com)  
             [LinkedIn](https://www.linkedin.com/in/anish-johnson-594110208/)""")
     if choice == "Home":
         html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
@@ -74,8 +68,7 @@ def main():
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
         st.write("Click on start to use webcam and detect your face emotion")
-        webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
-                        video_processor_factory=Faceemotion)
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
     elif choice == "About":
         st.subheader("About this app")
@@ -87,19 +80,12 @@ def main():
         st.markdown(html_temp_about1, unsafe_allow_html=True)
 
         html_temp4 = """
-                    <div style="background-color:#98AFC7;padding:10px">
-
-                    <h4 style="color:white;text-align:center;">
-                    This Application is developed by Anish Johnson using Streamlit Framework, 
-                    Opencv, Tensorflow and Keras library for demonstration purpose. 
-                    If you have any suggestion or wnat to comment just write a mail at anishjohnson05@gmail.com.
-                    </h4>
-
-                    <h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
-                    </div>
-                    <br></br>
-                    <br></br>
-                    """
+                             		<div style="background-color:#98AFC7;padding:10px">
+                             		<h4 style="color:white;text-align:center;">This Application is developed by Anish Johnson using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at anishjohnson05@gmail.com. </h4>
+                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
+                             		</div>
+                             		<br></br>
+                             		<br></br>"""
 
         st.markdown(html_temp4, unsafe_allow_html=True)
 
