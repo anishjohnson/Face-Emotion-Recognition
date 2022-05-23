90a5f5306f7bd27ad68d1a8d745c2450115708af
anish johnson <anishjohnson05@gmail.com>
2022-05-23 10:59:56 +0530

    Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,19 +29,21 @@ class Faceemotion(VideoTransformerBase):
         faces = face_cascade.detectMultiScale(
             image=img_gray, scaleFactor=1.3, minNeighbors=5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(255, 0, 0), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),4)
+            roi_gray = img_gray[y:y+h,x:x+w]
             roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+
             if np.sum([roi_gray]) != 0:
                 roi = roi_gray.astype('float') / 255.0
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
+                
                 prediction = classifier.predict(roi)[0]
                 maxindex = int(np.argmax(prediction))
                 finalout = emotion_labels[maxindex]
                 output = str(finalout)
-            label_position = (x, y)
+            label_position = (x, y-10)
+
             cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
         return img
